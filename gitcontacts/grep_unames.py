95b51e81266c02e3969a8f22eb2c8b1cdf9563c2
JudeSafo <jude.safo@gmail.com>
2021-12-07 22:14:20 -0500

    scrape contacts

---
+++ gitcontacts/grep_unames.py
@@ -0,0 +1,10 @@
+import sys
+
+file_name  = sys.argv[1]
+f = open(file_name)
+lines = f.readlines()
+uname = [urls.split("/")[-2] for urls in lines]
+names = open('names.txt','w')
+for name in uname:
+	names.write(name + "\n")
+names.close()
