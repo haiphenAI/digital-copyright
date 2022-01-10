c79b2cbda2c4a4f22d2da9b31d176541f01016fe
JudeSafo <jude.safo@gmail.com>
2022-01-10 07:10:18 -0500

    refactor

--- gitcontacts/gitunames.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-top_trending_repos=`echo  curl -G https://api.github.com/search/repositories --data-urlencode "sort=stars" --data-urlencode "order=desc" --data-urlencode "q=language:java"  --data-urlencode "q=created:>`date -v-7d '+%Y-%m-%d'`" | jq ".items[] | {name, description, language, watchers_count, html_url}"`
-echo -e $top_trending_repos
